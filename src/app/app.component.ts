42484a871d295fc25dae10589b788b64f14662e1
Jeisson Romero <jeisson.romerosophososlutions.com>
2022-04-08 16:16:13 -0500

    Ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public cadena: string[] = ['0', '1'];
+  public total: string[] = [];
+  public temporal: string[] = [];
+  public contador = 0;
+
+  public onRecursive(array: string[]) {
+    let arrayNew: string[] = [];
+    array.forEach((datos) => {
+      const data1 = datos + '0';
+      const data2 = datos + '1';
+      arrayNew.push(data1);
+      arrayNew.push(data2);
+    });
+    return arrayNew;
+  }
+
+  ngOnInit(): void {
+    this.temporal = this.onRecursive(this.cadena);
+  }
+
+  public onAdd(entrada: string[]) {
+    console.log(entrada);
+    this.contador = 0;
+    this.temporal = this.onRecursive(entrada);
+    this.temporal.map((data) => {
+      if (data.includes('111')) {
+        this.contador++;
+      }
+    });
+  }
 }
